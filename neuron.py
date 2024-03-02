41023d544c9e993212ff62802cd477fa61bfae1b
Nikhil Solanke <nikhilsolanke5@gmail.com>
2024-03-02 22:43:40 +0530

    Create a class

--- neuron.py
+++ neuron.py
@@ -1,15 +1,30 @@
 import numpy as np
 
-inputs = np.array([1, 2, 3, 2.5])
+np.random.seed(0)
 
-weights = np.array([
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87],
-]).T
+X = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8]
+]
 
-biases = np.array([2, 3, 0.5])
 
-output = np.dot(inputs, weights) + biases
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_neurons, n_inputs)
+        self.biases = np.zeros((1, n_neurons))
 
-print(output)
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights.T) + self.biases
+
+
+layer1 = Layer(4, 5)
+layer2 = Layer(5, 3)
+layer3 = Layer(3, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+layer3.forward(layer2.output)
+
+print(layer3.output)
